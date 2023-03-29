966ae917f29b1da24f6ec235fa1c4acb55454528
Valeria Dubina <valeria.dubina@isa.utm.md>
2023-03-29 12:13:25 +0300

    Add One to One strategy

--- src/main/scala/PoolSupervisor.scala
+++ src/main/scala/PoolSupervisor.scala
@@ -1,5 +1,6 @@
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
-import akka.routing.RoundRobinPool
+import akka.actor.SupervisorStrategy.{Escalate, Restart}
+import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, Props}
+import akka.routing.{DefaultResizer, RoundRobinPool}
 
 class PoolSupervisor(numPools: Int, actorClass: Class[_ <: Actor], emotionsMap: Map[String, Int])(implicit system: ActorSystem) extends Actor with ActorLogging{
   override def preStart(): Unit = {
@@ -10,7 +11,23 @@ class PoolSupervisor(numPools: Int, actorClass: Class[_ <: Actor], emotionsMap: 
     log.info(s"Pool Supervisor ${self.path.name} stopped")
   }
 
+  override val supervisorStrategy: OneForOneStrategy = OneForOneStrategy() {
+    case e: Exception =>
+      log.error(s"Actor ${sender().path.name} in pool ${sender().path.parent.name} failed with $e, restarting...")
+      Restart
+    case _ => Escalate
+  }
+
+  val resizer: DefaultResizer = DefaultResizer(
+    lowerBound = 3,
+    upperBound = 5,
+    backoffThreshold = 0.3,
+    messagesPerResize = 10,
+  )
+
   private val router = system.actorOf(RoundRobinPool(numPools)
+    .withResizer(resizer)
+    .withSupervisorStrategy(supervisorStrategy)
     .props(Props(actorClass, emotionsMap)))
 
   def receive: Receive = {
