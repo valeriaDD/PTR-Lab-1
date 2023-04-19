12b2a7136a149183ac5e4ef1247bd56b852f94e9
Valeria Dubina <valeria.dubina@isa.utm.md>
2023-04-19 20:03:05 +0300

    Add init database script

--- db/init-scripts.sql
+++ db/init-scripts.sql
@@ -1,15 +1,15 @@
 create
 extension hstore;
 create schema tweets;
+create table if not exists tweets."User" (
+     "id" VARCHAR NOT NULL PRIMARY KEY,
+     "name" VARCHAR
+);
+
 create table if not exists tweets."Tweet" (
-    "id" BIGSERIAL NOT NULL PRIMARY KEY,
-    "user_id" VARCHAR,
+    "id" VARCHAR NOT NULL PRIMARY KEY,
+    "user_id" VARCHAR REFERENCES tweets."User" (id),
     "text" VARCHAR,
     "engagement_ratio" VARCHAR,
     "sentimental_score" VARCHAR
-);
-
-create table if not exists tweets."User" (
-  "id" VARCHAR NOT NULL PRIMARY KEY,
-  "name" VARCHAR
 );
